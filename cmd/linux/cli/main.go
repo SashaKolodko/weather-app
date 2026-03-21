884b790074c45a5b82c0532129b8c84c4aa1f0e8
SashaKolodko <kolodkoa878@gmail.com>
2026-03-21 12:54:34 +0300

    fixed bags

--- cmd/linux/cli/main.go
+++ cmd/linux/cli/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "log"
     "github.com/SashaKolodko/weather-app/internal/pkg/app/cli"
     "github.com/SashaKolodko/weather-app/pkg/logger"
 )
@@ -11,7 +10,6 @@ func main() {
     app := cli.New(log)
     
     if err := app.Run(); err != nil {
-        log.Error(fmt.Sprintf("Application failed: %s", err.Error()))
-        log.Fatal(err)
+        log.Error("Application failed: " + err.Error())
     }
 }
